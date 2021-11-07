fb89edbb5951982ff82a9a0cf3a8f30e80d5d2e5
Domza64 <juricicdominik@protonmail.com>
2021-11-07 17:24:19 +0100

    Fix typo in RockTumbler.java

--- src/main/java/mysticgemstones/mysticgemstones/block/RockTumbler.java
+++ src/main/java/mysticgemstones/mysticgemstones/block/RockTumbler.java
@@ -24,9 +24,9 @@ public class RockTumbler extends Block {
         ItemStack itemStack = player.getStackInHand(hand);
         // Should probably use method overloading or variables or something like that for these if statments so there is no duplicate code....
         // Once in future hopefully...
-        if (!world.isClient && itemStack.isOf(MysticGemstonesItem.RAW_AQUAMARIN)) {
+        if (!world.isClient && itemStack.isOf(MysticGemstonesItem.RAW_AQUAMARINE)) {
             player.getStackInHand(hand).setCount(player.getStackInHand(hand).getCount() - 1);
-            Block.dropStack(world, pos, new ItemStack(MysticGemstonesItem.AQUAMARIN_ITEM));
+            Block.dropStack(world, pos, new ItemStack(MysticGemstonesItem.AQUAMARINE_ITEM));
             world.playSound(null, pos, SoundEvents.BLOCK_GRINDSTONE_USE, SoundCategory.BLOCKS, 1f, 1f);
         }
         if (!world.isClient && itemStack.isOf(MysticGemstonesItem.RAW_ALEXANDRITE)) {
