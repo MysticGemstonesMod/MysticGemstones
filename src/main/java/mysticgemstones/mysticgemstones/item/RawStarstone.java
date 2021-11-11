4103789561fb1c7da17cee48c302102ae0107c17
Domza64 <juricicdominik@protonmail.com>
2021-11-11 21:06:01 +0100

    Fixed Raw Starstone texture not loading by removing shiny/notShiny texture thing since notShiny texture was never used

--- src/main/java/mysticgemstones/mysticgemstones/item/RawStarstone.java
+++ src/main/java/mysticgemstones/mysticgemstones/item/RawStarstone.java
@@ -1,11 +1,9 @@
 package mysticgemstones.mysticgemstones.item;
 
-import net.fabricmc.fabric.api.object.builder.v1.client.model.FabricModelPredicateProviderRegistry;
 import net.minecraft.block.Block;
 import net.minecraft.entity.Entity;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
-import net.minecraft.util.Identifier;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 
@@ -18,18 +16,9 @@ public class RawStarstone extends Item {
     public void inventoryTick(ItemStack stack, World world, Entity entity, int slot, boolean selected) {
 
         if (world.getTimeOfDay() > 22350 || world.getTimeOfDay() < 21980) {
-            setItemModel(0.0F);
             stack.setCount(stack.getCount() - 1);
             BlockPos pos = entity.getBlockPos();
             Block.dropStack(world, pos, new ItemStack(MysticGemstonesItem.STARSTONE_DUST));
         }
-        else {
-            setItemModel(1.0F);
-        }
-    }
-
-    public void setItemModel(float number) {
-        FabricModelPredicateProviderRegistry.register(MysticGemstonesItem.RAW_STARSTONE,
-                new Identifier("shining"), (itemStack, clientWorld, livingEntity, hmmmm) -> number);
     }
 }
