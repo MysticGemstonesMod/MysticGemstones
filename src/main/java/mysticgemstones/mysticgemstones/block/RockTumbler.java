ad6ce32f65a2db761224f4e2e6fe712836d9e002
Domza64 <juricicdominik@protonmail.com>
2021-11-12 21:44:07 +0100

    Added 20% chance of Rock Tumbler breaking item

--- src/main/java/mysticgemstones/mysticgemstones/block/RockTumbler.java
+++ src/main/java/mysticgemstones/mysticgemstones/block/RockTumbler.java
@@ -44,15 +44,21 @@ public class RockTumbler extends Block {
     }
 
     private void polishGem(PlayerEntity player, Hand hand, World world, BlockPos pos, Item item) {
+        if (rng() >= 9) {   // 20% chance of item breaking
+            player.getStackInHand(hand).setCount(player.getStackInHand(hand).getCount() - 1);
+            world.playSound(null, pos, SoundEvents.ENTITY_ITEM_BREAK, SoundCategory.BLOCKS, 1f, 1f);
+        }
+        else {
         world.playSound(null, pos, SoundEvents.BLOCK_GRINDSTONE_USE, SoundCategory.BLOCKS, 1f, 1f);
         if (rng() <= 2) {   // 0 = 0%, 1 = 10%, 2 = 20%, 3 = 30% etc...up to 10 = 100% chance to polish item with 1 mouse click
             player.getStackInHand(hand).setCount(player.getStackInHand(hand).getCount() - 1);
             Block.dropStack(world, pos, new ItemStack(item));
+            }
         }
     }
 
     private int rng() {
-        int max = 10;
+        int max = 11;
         int min = 1;
         int number = new SecureRandom().nextInt(max - min) + min;
         return number;
