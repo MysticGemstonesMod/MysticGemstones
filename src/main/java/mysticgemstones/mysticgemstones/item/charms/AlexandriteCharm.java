c409591c959ab74f5dabaa640bf65c39d94a41f8
Domza64 <juricicdominik@protonmail.com>
2021-11-21 17:21:41 +0100

    Alexandrite Charm now has functionality

--- src/main/java/mysticgemstones/mysticgemstones/item/charms/AlexandriteCharm.java
+++ src/main/java/mysticgemstones/mysticgemstones/item/charms/AlexandriteCharm.java
@@ -1,9 +1,37 @@
 package mysticgemstones.mysticgemstones.item.charms;
 
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.text.LiteralText;
+import net.minecraft.world.World;
 
 public class AlexandriteCharm extends Item {
     public AlexandriteCharm(Settings settings) {
         super(settings);
+    }
+
+    private int isSet = 0;
+    int staticFoodLevel;
+
+    @Override
+    public void inventoryTick(ItemStack stack, World world, Entity entity, int slot, boolean selected) {
+        super.inventoryTick(stack, world, entity, slot, selected);
+
+        PlayerEntity player = (PlayerEntity)entity;
+
+        if (isSet == 2 || staticFoodLevel < player.getHungerManager().getFoodLevel()) {
+            staticFoodLevel = player.getHungerManager().getFoodLevel();
+            isSet = 0;
+        }
+        if (staticFoodLevel - 1 > player.getHungerManager().getFoodLevel()) {
+            player.getHungerManager().add(1, 0F);
+            // Should probably remove player message at some point.
+            if (isSet == 1) {
+                player.sendMessage(new LiteralText("+ 0.5 free chicken MC nugget"), false);
+            }
+            isSet++;
+        }
     }
 }
